92b0f6b9c7b53ea733159e9c2fcb4beb5d58a3f4
benjaminJ <kamikillerto@gmail.com>
2017-01-13 13:05:33 +0100

    Fix tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,10 +1,4 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
+import { assert } from 'chai';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -12,10 +6,10 @@ import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+describe("Extension Tests", () => {
 
     // Defines a Mocha unit test
-    test("Something 1", () => {
+    it("Something 1", () => {
         assert.equal(-1, [1, 2, 3].indexOf(5));
         assert.equal(-1, [1, 2, 3].indexOf(0));
     });
