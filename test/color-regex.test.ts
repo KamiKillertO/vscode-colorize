a9a7ea2c02a0e8dec6cb4b78d9e3bf273bf9c4fb
benjaminJ <kamikillerto@gmail.com>
2017-02-02 19:10:51 +0100

    Added new function for rgb colors extractions

--- test/color-regex.test.ts
+++ test/color-regex.test.ts
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 
-import { HEXA_COLOR } from '../src/color-regex';
+import { HEXA_COLOR, RGB_COLOR} from '../src/color-regex';
 
 // Defines a Mocha test suite to group tests of similar kind together
 describe("Test CSS hexa shorthand color Regex", () => {
@@ -52,5 +52,22 @@ describe("Test CSS hexa color Regex", () => {
     assert.notOk('#fffffg'.match(HEXA_COLOR));
     assert.notOk('#ffffffg'.match(HEXA_COLOR));
     assert.notOk('#fffff'.match(HEXA_COLOR));
+  });
+});
+describe("Test rgb(a) color Regex", () => {
+  it('Should match a simple rgb color', function () {
+    assert.ok('rgb(123,123,123)'.match(RGB_COLOR));
+  });
+  it('Should match a simple rgba color', function () {
+    assert.ok('rgba(123,123,123, 0)'.match(RGB_COLOR));
+    assert.ok('rgba(123,123,123, 0.3)'.match(RGB_COLOR));
+    assert.ok('rgba(123,123,123, 1)'.match(RGB_COLOR));
+  });
+
+  it('Should match with different characters at the end', function () {
+    assert.ok('rgb(123,123,123) '.match(RGB_COLOR));
+    assert.ok('rgb(123,123,123),'.match(RGB_COLOR));
+    assert.ok('rgb(123,123,123);'.match(RGB_COLOR));
+    assert.ok('rgb(123,123,123)\n'.match(RGB_COLOR));
   });
 });
