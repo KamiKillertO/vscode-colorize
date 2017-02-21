c42fe53da7562e85642a003ec62f8219ee7f8071
benjaminJ <kamikillerto@gmail.com>
2017-02-21 13:46:10 +0100

    Fix background update for line deletion or addition

--- src/extension.ts
+++ src/extension.ts
@@ -49,39 +49,52 @@ function generateTextDocumentContentChange(line: number, text: string): TextDocu
 }
 
 function mutEditedLIneForDeletion(editedLine: TextDocumentContentChangeEvent[]): TextDocumentContentChangeEvent[] {
-  let newEditedLine: TextDocumentContentChangeEvent[] = [];
-  let startLine = 0;
-  let before = 0;
-  editedLine.reverse();
-  editedLine.forEach(line => {
-    startLine = line.range.start.line + before;
-    for (let i = line.range.start.line; i <= line.range.end.line; i++) {
-      newEditedLine.push(generateTextDocumentContentChange(i, line.text));
-      before--;
-    }
-    before++;
-  });
-  return newEditedLine.reverse();
+  // let newEditedLine: TextDocumentContentChangeEvent[] = [];
+  // let startLine = 0;
+  // let before = 0;
+  // editedLine.reverse();
+  // editedLine.forEach(line => {
+  //   startLine = line.range.start.line + before;
+  //   for (let i = line.range.start.line; i <= line.range.end.line; i++) {
+  //     newEditedLine.push(generateTextDocumentContentChange(i, line.text));
+  //     before--;
+  //   }
+  //   before++;
+  // });
+  // return newEditedLine;
+  return editedLine;
 }
 
 function mutEditedLIne(editedLine: TextDocumentContentChangeEvent[]): TextDocumentContentChangeEvent[] {
+
   let newEditedLine: TextDocumentContentChangeEvent[] = [];
   let startLine = 0;
   let before = 0;
   editedLine.reverse();
   editedLine.forEach(line => {
+    // debugger
+    let a = line.text.match(/\n/g);
     startLine = line.range.start.line + before;
-    if (line.text === "\n") {
-      newEditedLine.push(line);
-    } else {
-      line.text.split(/\n/).map(text => {
-        newEditedLine.push(generateTextDocumentContentChange(startLine++, line.text));
+    // if (line.text.match(/\n/g).length === 1 ) {
+    //   if (line.text.match(/\n$/)) {
+    //     newEditedLine.push(generateTextDocumentContentChange(++startLine, line.text));
+    //   } else {
+    //     newEditedLine.push(line);
+    //   }
+    // } else {
+      line.text.split(/\n/).map((text, i, array) => {
+        if (i === 0 && text === '' && array.length === 1) {
+          startLine++;
+        } else {
+          newEditedLine.push(generateTextDocumentContentChange(startLine++, line.text));
+        }
         before++;
       });
+      // newEditedLine.push(generateTextDocumentContentChange(startLine, line.text));
       before--;
-    }
+    // }
   });
-  return newEditedLine.reverse();
+  return newEditedLine;
 }
 
 function updatePositionsForDeletion(range, positions) {
@@ -90,7 +103,7 @@ function updatePositionsForDeletion(range, positions) {
     if (position.newPosition === null) {
       return;
     }
-    if (position.oldPosition >= range.start.line && position.oldPosition < (range.end.line + 1)) {
+    if (position.oldPosition > range.start.line && position.oldPosition <= range.end.line) {
       position.newPosition = null;
       return;
     }
@@ -109,12 +122,14 @@ function handleLineRemoved(editedLine: TextDocumentContentChangeEvent[], positio
   editedLine.forEach((line) => {
     positions = updatePositionsForDeletion(line.range, positions);
   });
-  editedLine.reverse();
+  // editedLine.reverse();
 
-  return mutEditedLIneForDeletion(editedLine);
+  // return mutEditedLIneForDeletion(editedLine);
+  return editedLine;
 }
 
 function handleLineDiff(editedLine: TextDocumentContentChangeEvent[], context, diffLine: number) {
+  // debugger;
   let positions = mapKeysToArray(context.deco).map(position => Object({
     oldPosition: position,
     newPosition: position
@@ -129,6 +144,9 @@ function handleLineDiff(editedLine: TextDocumentContentChangeEvent[], context, d
     if (position.newPosition === null) {
       context.deco.get(position.oldPosition).forEach(decoration => decoration.dispose());
       return false;
+    }
+    if (Math.abs(position.oldPosition - position.newPosition) > Math.abs(diffLine)) {
+      position.newPosition = position.oldPosition + diffLine;
     }
     return true;
   });
@@ -146,19 +164,36 @@ function handleLineDiff(editedLine: TextDocumentContentChangeEvent[], context, d
 
 function handleLineAdded(editedLine: TextDocumentContentChangeEvent[], position) {
   editedLine = mutEditedLIne(editedLine);
-  editedLine.forEach((line) => {
-    position.forEach(position => {
-      if (position.oldPosition > line.range.start.line) {
-        position.newPosition = position.newPosition + 1;
-      }
+  // debugger;
+  // if (editedLine.length === 1) {
+  //   position.forEach(position => {
+  //     if (position.oldPosition >= editedLine[0].range.start.line) {
+  //       position.newPosition = position.newPosition + 1;
+  //     }
+  //   });
+  // } else {
+    editedLine.forEach((line) => {
+      position.forEach(position => {
+         if (position.newPosition >= line.range.start.line) {
+        //if (position.newPosition > line.range.start.line) {
+          position.newPosition = position.newPosition + 1;
+        }
+      });
     });
-  });
+  // }
+  // editedLine.forEach((line) => {
+  //   position.forEach(position => {
+  //     if (position.oldPosition > line.range.start.line) {
+  //       position.newPosition = position.newPosition + 1;
+  //     }
+  //   });
+  // });
   return editedLine;
 }
 
 function updateDecorations(editedLine: TextDocumentContentChangeEvent[], context, cb: Function) {
   let diffLine = context.current_editor.document.lineCount - context.nbLine;
-
+  let positions;
   if (diffLine !== 0) {
     editedLine = handleLineDiff(editedLine, context, diffLine);
     context.nbLine = context.current_editor.document.lineCount;
@@ -167,6 +202,7 @@ function updateDecorations(editedLine: TextDocumentContentChangeEvent[], context
 }
 
 function checkDecorationForUpdate(editedLine: TextDocumentContentChangeEvent[], context, cb) {
+  // debugger;
   Promise.all(
     editedLine.map(({range}: TextDocumentContentChangeEvent) => {
       if (context.deco.has(range.start.line)) {
@@ -175,8 +211,12 @@ function checkDecorationForUpdate(editedLine: TextDocumentContentChangeEvent[], 
         });
       }
       context.deco.set(range.start.line, []);
-      return ColorUtil.findColors(context.current_editor.document.lineAt(range.start.line).text)
-              .then(colors => generateDecorations(colors, range.start.line, context));
+      try { // not really good bu still avoid lots of issues
+        return ColorUtil.findColors(context.current_editor.document.lineAt(range.start.line).text)
+                .then(colors => generateDecorations(colors, range.start.line, context));
+      } catch(e) { //use promise catch instead?
+        return context; 
+      }
     })
   ).then(() => decorateEditor(context))
    .then(cb);
@@ -193,7 +233,7 @@ function initDecorations(context, cb) {
   Promise.all(context.current_editor.document.getText()
                                   .split(/\n/)
                                   .map((text, index) => Object({"text": text, "line": index}))
-                                  .filter(line => line.text !== "")
+                                  // .filter(line => line.text !== "")
                                   .map(line => ColorUtil.findColors(line.text)
                                               .then(colors => generateDecorations(colors, line.line, context))))
                                   .then(() => decorateEditor(context))
@@ -213,24 +253,28 @@ function initDecorations(context, cb) {
 }
 
 function generateDecorations(colors: Color[], line, context) {
+  // debugger;
   colors.forEach((color) => {
     let startPos = new Position(line, color.positionInText);
     let endPos = new Position(line, color.positionInText + color.value.length);
     let range = new Range(startPos, endPos);
     if (context.deco.has(line)) {
-      context.deco.set(line, context.deco.get(line).concat([new ColorDecoration(range, color)]));
+      context.deco.set(line, context.deco.get(line).concat([new ColorDecoration(/*range, */color)]));
     } else {
-      context.deco.set(line, [new ColorDecoration(range, color)]);
+      context.deco.set(line, [new ColorDecoration(/*range, */color)]);
     }
   });
   return context; // return colors instead?
 }
 
-function decorateEditor(context) {
+function decorateEditor(context) { // should use colors and not deco otherwise range need to be updated 
   let it = context.deco.entries();
   let tmp = it.next();
   while (!tmp.done) {
-    tmp.value[1].forEach(decoration => context.current_editor.setDecorations(decoration.decoration, [decoration.textPosition]));
+    // debugger;
+    let line = tmp.value[0];
+    // tmp.value[1].forEach(decoration => context.current_editor.setDecorations(decoration.decoration, [decoration.textPosition]));
+    tmp.value[1].forEach(decoration => context.current_editor.setDecorations(decoration.decoration, [decoration.generateRange(line)]));
     tmp = it.next();
   }
   return;
