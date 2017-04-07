c8d2cf0363665a0bc0dd214cf93c9e6684b2acd9
benjaminJ <kamikillerto@gmail.com>
2017-04-07 08:39:49 +0200

    Fixing tests

--- test/extractors/browser-extrator.test.ts
+++ test/extractors/browser-extrator.test.ts
@@ -6,19 +6,27 @@ import { REGEXP } from '../../src/util/extractors/browser-extractor';
 describe("Test browser predefined color Regex", () => {
 
   it('white should match', function () {
-    assert.ok('white'.match(REGEXP));
+    assert.ok(' white'.match(REGEXP));
+    assert.ok(',white'.match(REGEXP));
+    assert.ok('(white'.match(REGEXP));
+    assert.ok(':white'.match(REGEXP));
   });
   it('Should not match inside a string', function() {
     assert.notOk('"white"'.match(REGEXP));
   });
+  it('Should not match without a valid char before', function() {
+    assert.notOk('awhite'.match(REGEXP));
+    assert.notOk('-white'.match(REGEXP));
+    assert.notOk('$white'.match(REGEXP));
+  });
 
   it('Should match with different characters at the end', function () {
-    assert.ok('white'.match(REGEXP));
-    assert.ok('white '.match(REGEXP));
-    assert.ok('white,'.match(REGEXP));
-    assert.ok('white;'.match(REGEXP));
-    assert.ok('white\n'.match(REGEXP));
-    assert.ok('white)\n'.match(REGEXP));
+    assert.ok(' white'.match(REGEXP));
+    assert.ok(' white '.match(REGEXP));
+    assert.ok(' white,'.match(REGEXP));
+    assert.ok(' white;'.match(REGEXP));
+    assert.ok(' white\n'.match(REGEXP));
+    assert.ok(' white)\n'.match(REGEXP));
   });
   it('Should not match', function () {
     assert.notOk('white-'.match(REGEXP));
