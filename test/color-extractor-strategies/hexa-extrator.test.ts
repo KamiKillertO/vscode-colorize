728fd5a74902c6166d2e71fe59679bf7f43395f5
BenjaminJ <kamikillerto@gmail.com>
2020-12-10 11:01:57 +0100

    style: fix tslint error

--- test/color-extractor-strategies/hexa-extrator.test.ts
+++ test/color-extractor-strategies/hexa-extrator.test.ts
@@ -51,8 +51,9 @@ describe('Test CSS hexa color Regex', () => {
   });
   it('Sould match inside a string', function() {
     assert.equal(regex_exec('"#ffffff"', REGEXP)[1], '#ffffff');
+    // tslint:disable-next-line
     assert.equal(regex_exec("'#ffffff'", REGEXP)[1], '#ffffff');
-    assert.equal(regex_exec("`#ffffff`", REGEXP)[1], '#ffffff');
+    assert.equal(regex_exec('`#ffffff`', REGEXP)[1], '#ffffff');
   });
   it('Should match with different characters at the end', function () {
     assert.equal(regex_exec('#ffffff ', REGEXP)[1], '#ffffff');
