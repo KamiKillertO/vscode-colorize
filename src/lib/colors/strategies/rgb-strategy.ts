2257c91b193c17a14e3045133eb853424c71d74c
BenjaminJ <kamikillerto@gmail.com>
2024-11-07 09:59:50 +0100

    feat: Add support for rgb values with %

--- src/lib/colors/strategies/rgb-strategy.ts
+++ src/lib/colors/strategies/rgb-strategy.ts
@@ -7,7 +7,7 @@ const R_RED = `(?:\\d{1,3}${DOT_VALUE}?|${DOT_VALUE})`;
 const R_GREEN = R_RED;
 const R_BLUE = R_RED;
 
-const RGB_NEW_SYNTAX = `rgba?\\(\\s*${R_RED}\\s*${R_GREEN}\\s*${R_BLUE}(?:\\s*\\/\\s*(?:\\d{1,3}${DOT_VALUE}?|${ALPHA})?%?)?\\)`;
+const RGB_NEW_SYNTAX = `rgba?\\(\\s*${R_RED}%?\\s*${R_GREEN}%?\\s*${R_BLUE}%?(?:\\s*\\/\\s*(?:\\d{1,3}${DOT_VALUE}?|${ALPHA})?%?)?\\)`;
 const RGB_LEGACY_SYNTAX = `rgb\\(\\s*${R_RED}\\s*,\\s*${R_GREEN}\\s*,\\s*${R_BLUE}\\s*\\)`;
 const RGBA_LEGACY_SYNTAX = `rgba\\(\\s*${R_RED}\\s*,\\s*${R_GREEN}\\s*,\\s*${R_BLUE}\\s*,\\s*${ALPHA}\\s*\\)`;
 
@@ -24,24 +24,45 @@ function extractRGBA(value: string): number[] {
   const rgba_string = value
     .replace(/rgb(a){0,1}\(/, '')
     .replace(/\)/, '')
-    .replace(/%/g, '')
     .replace('/', ' ')
     .replaceAll(',', ' ');
-  return rgba_string.split(/\s+/).map((c) => parseFloat(c));
+  const values = rgba_string.split(/\s+/);
+
+  const [r, g, b] = values.slice(0, 3).map((v) => {
+    const value = parseFloat(v);
+
+    if (v.includes('%')) {
+      const percentage = value > 100 ? 100 : value;
+
+      return (255 * percentage) / 100;
+    }
+
+    return value;
+  });
+
+  const isRelativeAlpha = /%/.test(values[3]);
+
+  let a = parseFloat(values[3] ?? 1);
+
+  if (!isRelativeAlpha && a > 1) {
+    a = 1;
+  }
+
+  if (isRelativeAlpha && a > 100) {
+    a = 1;
+  }
+
+  if (a > 1) {
+    a = a / 100;
+  }
+  return [r, g, b, a];
 }
 
 function getColor(match: RegExpExecArray) {
   const value = match[1];
   const rgba = extractRGBA(value);
-  let alpha = rgba[3] ?? 1;
 
-  if (alpha > 100) {
-    alpha = 1;
-  }
-
-  if (alpha > 1) {
-    alpha = alpha / 100;
-  }
+  const alpha = rgba[3] ?? 1;
 
   const rgb = rgba.slice(0, 3) as [number, number, number];
   // Check if it's a valid rgb(a) color
